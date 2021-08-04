1850932ec1ad4195dc7b8c71f0dc1c930be3723c
MrJin <corrobolea@gmail.com>
2021-08-04 17:42:38 +0900

    계산 로직 작업 중

--- app/src/main/java/com/example/calculatorproject/MainActivity.kt
+++ app/src/main/java/com/example/calculatorproject/MainActivity.kt
@@ -2,12 +2,67 @@ package com.example.calculatorproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val expressionTextView: TextView by lazy {
+        findViewById<TextView>(R.id.expressionTextView)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById<TextView>(R.id.resultTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    fun buttonClicked(v: View) {
+        when(v.id) {
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+            R.id.buttonPlus -> operatorButtonClicked("+")
+            R.id.buttonMinus -> operatorButtonClicked("-")
+            R.id.buttonMulti -> operatorButtonClicked("*")
+            R.id.buttonDivider -> operatorButtonClicked("/")
+            R.id.buttonModulo -> operatorButtonClicked("%")
+        }
+    }
+
+    fun numberButtonClicked(number: String) {
+        val expressionText = expressionTextView.text.split("")
+        if(expressionText.isNotEmpty() && expressionText.last().length >= 15) {
+            Toast.makeText(this, "15자리 까지만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun operatorButtonClicked(operator: String) {
+
+    }
+
+    fun clearButtonClicked(v: View) {
+
+    }
+
+    fun historyButtonButtonClicked(v: View) {
+
+    }
+
+    fun resultButtonClicked(v: View) {
 
     }
 }
